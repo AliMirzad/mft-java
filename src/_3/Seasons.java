d4acafd4b083a14a86fc93a3a9438cb0511930ac
beast <ali.m.codecraft@gmail.com>
2024-10-11 22:44:18 +0330

    ne

---
+++ src/_3/Seasons.java
@@ -0,0 +1,13 @@
+package _3;
+
+public enum Seasons {
+    WINTER(1), SPRING(2), SUMMER(3), AUTUMN(4);
+
+    private int value;
+
+    Seasons(int index) {
+        this.value = index;
+    }
+
+
+}
